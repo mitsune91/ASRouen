820132b10f6c98cc9a01d2e34c6fc2e3a066d3db
Thomas Giotti <thomas.giotti@modiscloud.net>
2021-03-09 18:17:06 +0100

    first commit

---
+++ back_asrouen/src/main/java/com/association/asrouen/AsrouenApplication.java
@@ -0,0 +1,13 @@
+package com.association.asrouen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrouenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrouenApplication.class, args);
+	}
+
+}
---
+++ back_asrouen/src/test/java/com/association/asrouen/AsrouenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.association.asrouen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrouenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
