3c8a732b1108b15c64278424b1b0e2038f9287a1
Thomas Giotti <thomas.giotti@modiscloud.net>
2021-03-12 15:18:34 +0100

    restore defaullt Spring & react native project

---
+++ backend/aston/src/main/java/groupe4/aston/AstonApplication.java
@@ -0,0 +1,13 @@
+package groupe4.aston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstonApplication.class, args);
+	}
+
+}
---
+++ backend/aston/src/test/java/groupe4/aston/AstonApplicationTests.java
@@ -0,0 +1,13 @@
+package groupe4.aston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
